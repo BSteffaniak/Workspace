36ff2c24041c40618e4d32bc4a87c94059f56439
Braden Steffaniak <BradenSteffaniak@gmail.com>
2013-05-24 15:49:00 -0500

    Phone updates

---
+++ JFoxyLib/src/net/foxycorndog/jfoxylib/components/MenuItem.java
@@ -0,0 +1,22 @@
+public class MenuItem
+{
+	private	String	name;
+	
+	/**
+	 * Create a MenuItem with the specified name.
+	 */
+	public MenuItem(String name)
+	{
+		this.name = name;
+	}
+	
+	/**
+	 * Get the name of the MenuItem.
+	 * 
+	 * @return The name of the MenuItem.
+	 */
+	public String getName()
+	{
+		return name;
+	}
+}
